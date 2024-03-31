b1b84379b12c49af500560b03bb7c994601f6c56
Yeni0325 <yekim0325@gmail.com>
2024-03-31 17:17:01 +0900

    Extracting state logic into a reducer

--- src/02.ManagingState/App.jsx
+++ src/02.ManagingState/App.jsx
@@ -180,8 +180,8 @@ import "./App.css";
 // }
 
 // -------------------------------------------------------------------------------
-import ContactList from "./ContactList.jsx";
-import Chat from "./Chat.jsx";
+// import ContactList from "./ContactList.jsx";
+// import Chat from "./Chat.jsx";
 
 // < 2-4. Preserving and resetting state : state 보존 및 재설정  >
 /*
@@ -201,22 +201,134 @@ import Chat from "./Chat.jsx";
       }
     2. 각 컴포넌트에 key로 명시적인 아이덴티티 부여하기
 */
-export default function Messenger() {
-  const [to, setTo] = useState(contacts[0]);
-  return (
-    <div>
-      <ContactList
-        contacts={contacts}
-        selectedContact={to}
-        onSelect={contact => setTo(contact)}
+// export default function Messenger() {
+//   const [to, setTo] = useState(contacts[0]);
+//   return (
+//     <div>
+//       <ContactList
+//         contacts={contacts}
+//         selectedContact={to}
+//         onSelect={contact => setTo(contact)}
+//       />
+//       <Chat key={to.id} contact={to} />
+//     </div>
+//   )
+// }
+
+// const contacts = [
+//   { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+//   { id: 1, name: 'Alice', email: 'alice@mail.com' },
+//   { id: 2, name: 'Bob', email: 'bob@mail.com' }
+// ];
+
+// -------------------------------------------------------------------------------
+import { useReducer } from "react"; 
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
+import tasksReducer from "./taskReducer";
+
+// < 2-4. Extracting state logic into a reducer : state 로직을 reducer로 추출하기   >
+/*
+  컴포넌트가 커질수록 여기저기 흩어져 있는 state 로직의 양도 늘어난다.
+  복잡성을 줄이고 모든 로직을 접근하기 쉽게 한 곳에 모으려면, state 로직을 컴포넌트 외부의 reducer라고 하는 단일 함수로 옮길 수 있다.
+
+  * Reducer는 state를 관리하는 다른 방법이다.
+  useState에서 useReducer로 마이그레이션하는 방법은 세 단계로 진행된다.
+    1. state를 설정하는 것에서 action들을 전달하는 것으로 변경하기
+    2. reducer 함수 작성하기
+    3. 컴포넌트에서 reducer 사용하기
+
+  * reducer 형태 *
+  const [state, dispatch] = useReducer(reducer, initalState, init);
+    1. state : 컴포넌트에서 사용할 state
+    2. dispatch 
+      - 첫번째 인자인 reducer 함수를 실행시킨다.
+      - 컴포넌트 내에서 state의 업데이트를 하기 위해 사용하는 함수
+    3. reducer 함수
+      - 컴포넌트 외부에서 state를 업데이트 하는 함수
+      - 현재 state, action 객체를 인자로 받아, 기존의 state를 대체하여 새로운 state를 반환하는 함수
+    4. initalState
+      - 초기 state
+    5. init
+      - 초기 함수 (초기 state를 조금 지연해서 생성하기 위해 사용)
+
+  dispatch 함수에 넣어준 객체를 “action” 이라고 한다.
+  
+  * reducer 안에서는 switch 구문을 사용하는 게 일반적이다.
+
+  * useReducer Hook은 useState와 비슷하다. 
+  초기 state 값을 전달해야 하며, 그 결과로 state 값과 state 설정자 함수(useReducer의 경우 dispatch 함수)를 반환한다. 하지만 조금 다른 점이 있다.
+
+  * useReducer Hook은 두 개의 인자를 받는다.
+    1. reducer 함수
+    2. 초기 state
+  
+  * useReducer는 아래 내용을 반환한다.
+    1. state값
+    2. dispatch 함수 (사용자의 action을 reducer에 “전달”해주는 함수)
+
+  * useState와 useReducer 비교
+  1. 코드 크기 
+    : 일반적으로 useState를 사용하면 미리 작성해야 하는 코드가 줄어든다. useReducer를 사용하면 reducer 함수 와 action을 전달하는 부분 모두 작성해야 한다. 
+      하지만 많은 이벤트 핸들러가 비슷한 방식으로 state를 업데이트하는 경우 useReducer를 사용하면 코드를 줄이는 데 도움이 될 수 있다.
+  2. 가독성
+    : useState로 간단한 state를 업데이트 하는 경우 가독성이 좋다. 그렇지만 state의 구조가 더욱 복잡해지면, 컴포넌트의 코드의 양이 부풀어 오르고 한눈에 읽기 어려워질 수 있다. 
+      이 경우 useReducer를 사용하면 업데이트 로직이 어떻게 동작 하는지와 이벤트 핸들러를 통해 무엇이 일어났는지 를 깔끔하게 분리할 수 있다.
+  3. 디버깅
+    :  reducer는 컴포넌트에 의존하지 않는 순수한 함수입니다. 즉, 별도로 분리해서 내보내거나 테스트할 수 있다.
+
+  * reducer 잘 사용하기
+  : reducer는 반드시 순수해야한다. 즉, 입력 값이 같다면 결과 값도 항상 같아야 한다.
+  
+  Immer를 사용하여 간결한 reducer 작성할 수 있다.
+*/
+export default function TaskApp(){
+  const [tasks, dispatch] = useReducer(
+    tasksReducer ,
+    initialTasks
+  );
+
+  function handleAddTask(text){
+    dispatch({
+      type : 'added' ,
+      id : nextId++ ,
+      text : text ,
+    });
+  }
+
+  function handleChangeTask(task){
+    dispatch({
+      type : 'changed' ,
+      task : task ,
+    });
+  }
+
+  function handleDeleteTask(taskId){
+    dispatch({
+      type : 'deleted' ,
+      id : taskId ,
+    });
+  }
+
+  return(
+    <>
+      <h1>Prague itinerary</h1>
+      <AddTask 
+        onAddTask={handleAddTask}
       />
-      <Chat key={to.id} contact={to} />
-    </div>
-  )
+      <TaskList 
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
 }
 
-const contacts = [
-  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
-  { id: 1, name: 'Alice', email: 'alice@mail.com' },
-  { id: 2, name: 'Bob', email: 'bob@mail.com' }
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: 'Visit Kafka Museum', done: true },
+  { id: 1, text: 'Watch a puppet show', done: false },
+  { id: 2, text: 'Lennon Wall pic', done: false }
 ];
+
