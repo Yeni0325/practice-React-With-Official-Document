5fe0c08572d05d3a08e483c16edab41dd29bef96
Yeni0325 <yekim0325@gmail.com>
2024-04-04 21:01:02 +0900

    forwardRef

--- src/03.EscapeHatches/App.jsx
+++ src/03.EscapeHatches/App.jsx
@@ -117,39 +117,63 @@ import "./App.css";
 //   );
 // }
 
-export default function Stopwatch() {
-  const [startTime, setStartTime] = useState(null);
-  const [now, setNow] = useState(null);
-  const intervalRef = useRef(null);
+// export default function Stopwatch() {
+//   const [startTime, setStartTime] = useState(null);
+//   const [now, setNow] = useState(null);
+//   const intervalRef = useRef(null);
 
-  function handleStart() {
-    setStartTime(Date.now());
-    setNow(Date.now());
+//   function handleStart() {
+//     setStartTime(Date.now());
+//     setNow(Date.now());
 
-    clearInterval(intervalRef.current);
-    intervalRef.current = setInterval(() => {
-      setNow(Date.now());
-    }, 10);
-  }
+//     clearInterval(intervalRef.current);
+//     intervalRef.current = setInterval(() => {
+//       setNow(Date.now());
+//     }, 10);
+//   }
 
-  function handleStop() {
-    clearInterval(intervalRef.current);
-  }
+//   function handleStop() {
+//     clearInterval(intervalRef.current);
+//   }
 
-  let secondsPassed = 0;
-  if (startTime != null && now != null) {
-    secondsPassed = (now - startTime) / 1000;
-  }
+//   let secondsPassed = 0;
+//   if (startTime != null && now != null) {
+//     secondsPassed = (now - startTime) / 1000;
+//   }
 
-  return (
-    <>
-      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
-      <button onClick={handleStart}>
-        Start
-      </button>
-      <button onClick={handleStop}>
-        Stop
-      </button>
-    </>
-  );
+//   return (
+//     <>
+//       <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+//       <button onClick={handleStart}>
+//         Start
+//       </button>
+//       <button onClick={handleStop}>
+//         Stop
+//       </button>
+//     </>
+//   );
+// }
+
+// -------------------------------------------------------------------------------
+
+// < 3-2. Manipulating the DOM with Refs : ref로 DOM 조작하기 >
+/*
+
+*/
+import MyInput from './MyInput.jsx';
+
+export default function App(){
+	const inputRef = useRef();
+	
+	const focus = () => {
+		inputRef.current.focus();
+	};
+	
+	return(
+		<div>
+			{/*<input ref={inputRef} />*/}
+			<MyInput ref={inputRef}/>
+			<button onClick={focus}>집중</button>
+		</div>
+	);
 }
